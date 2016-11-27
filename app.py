40cd387fd0d00f8d2e6d1a3e17979bc26c676051
Maggie A Hewitt <maggie.a.hewitt@gmail.com>
2016-11-27 17:03:08 -0500

    Added basic routing using Flask

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, url_for, redirect
 app = Flask(__name__)
 
 
 @app.route("/")
 def index(name=None):
-    return render_template('index.html', name='Home')
+    return render_template('workout_form.html', name='Home')
+
+
+@app.route("/workout")
+def workout(name=None):
+    return render_template('workout_list.html', name='Workout')
+
+
+@app.route("/generate", methods=['POST'])
+def generate():
+    return redirect(url_for('workout'))
 
 
 if __name__ == "__main__":
