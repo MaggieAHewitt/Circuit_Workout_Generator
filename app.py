df95e0eb0af50fa5ada012177d418556d077f478
Maggie A Hewitt <maggie.a.hewitt@gmail.com>
2016-11-28 23:24:22 -0500

    Fix refresh issue because of an outstanding safari issue. Fix styling issues with button.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, url_for
+from flask import Flask, request, redirect, render_template, url_for, session
 from workout import *
 
 app = Flask(__name__)
@@ -11,7 +11,24 @@ def index(name=None):
     return render_template('workout_form.html', name='Home')
 
 
-@app.route("/workout", methods=['GET','POST'])
+@app.route("/refresh", methods=['GET', 'POST'])
+def refresh():
+    last_request = session['last_request']
+
+    if((last_request is None) or (request.method == 'GET')):
+        return redirect('/')
+
+    num_of_circuits = last_request.get('num_of_circuits')
+    num_of_exerecises = last_request.get('num_of_exerecises')
+    focus = last_request.get('focus')
+    no_weights = last_request.get('no_weights')
+    no_stability_ball = last_request.get('no_stability_ball')
+    no_machines = last_request.get('no_machines')
+
+    workout = Workout(all_exercises, int(num_of_circuits), int(num_of_exerecises), focus, no_weights, no_stability_ball, no_machines)
+    return render_template('workout_list.html', circuits=workout.circuits)
+
+@app.route("/workout", methods=['GET', 'POST'])
 def workout():
 
     if request.method == 'GET':
@@ -32,9 +49,14 @@ def workout():
     if request.form.get('exerciseMachinesAvailable'):
         no_machines = False
 
-    workout = Workout(all_exercises, int(num_of_circuits), int(num_of_exerecises), focus, no_weights, no_stability_ball, no_machines)
+    last_request = {'num_of_circuits': num_of_circuits, 'num_of_exerecises': num_of_exerecises, 'focus': focus, 'no_weights': no_weights, 'no_stability_ball': no_stability_ball, 'no_machines': no_machines}
 
+    session['last_request'] = last_request
+    workout = Workout(all_exercises, int(num_of_circuits), int(num_of_exerecises), focus, no_weights, no_stability_ball, no_machines)
     return render_template('workout_list.html', circuits=workout.circuits)
 
+app.secret_key = '\x96\xa9\xca\xb7.o\xe7:\xd4D\x04\xea\x98\xa9[#"\r`[v\xb9\xa3\xe9'
+
+# Remove for Python Hosting
 if __name__ == "__main__":
     app.run()
